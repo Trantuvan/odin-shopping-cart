ca01d2fcb2e8a64b7f291d7f2ef8978b3f286f0d
trantuvan <trantuvan.kan@gmail.com>
2023-03-24 22:12:35 +0700

    Config React Router Dom
    Pick router: createHashRouter
    Because:
    + Github gh-pages for deployment only supports hash routing
    + createHashRouter & createBrowserRouter are new supported data routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+
+import RootLayout from './layouts/RootLayout';
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <div>something wrong</div>,
+    // children: [
+    //   {
+    //     errorElement: <div>sth wrong</div>,
+    //     children: [
+    //       { index: true, element: <HomePage /> },
+    //       {
+    //         path: "products",
+    //         element: <ProductsPage />,
+    //         loader: productLoader,
+    //       },
+    //       { path: "*", element: <NoMatchPage /> },
+    //     ],
+    //   },
+    // ],
+  },
+]);
+
 function App() {
-  return null;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
