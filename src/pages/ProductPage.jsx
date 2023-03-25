9be13c477b29ec14cc863c03a90ea2d58e0d1635
trantuvan <trantuvan.kan@gmail.com>
2023-03-25 16:47:28 +0700

    Load products data for products route
    New feature of react-router-dom v6.4
    Loader func allow calling network when navigate to product route

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,24 +1,41 @@
+import { Suspense } from 'react';
 import clsx from 'clsx';
+import { Await, defer, useLoaderData } from 'react-router-dom';
 
 import styles from '../styles/ProductPage.module.css';
 import { SearchTool, FilterPrice, ProductItem } from '../components';
+import { getAllProducts } from '../apis';
 
 const ProductsPage = () => {
+  const { products } = useLoaderData();
+
   return (
     <div className={clsx(styles.productsContainer)}>
       <div className={clsx(styles.toolbar)}>
         <SearchTool />
         <FilterPrice />
       </div>
-      {/* <ul className={clsx(styles.productList)}>
-        {products.map((product) => (
-          <li key={product.id}>
-            <ProductItem product={product} />
-          </li>
-        ))}
-      </ul> */}
+      <ul className={clsx(styles.productList)}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Await resolve={products}>
+            {(resolvedProducts) =>
+              resolvedProducts.map((product) => (
+                <li key={product.id}>
+                  <ProductItem product={product} />
+                </li>
+              ))
+            }
+          </Await>
+        </Suspense>
+      </ul>
     </div>
   );
 };
 
+const loader = async () => {
+  const products = getAllProducts();
+  return defer({ products });
+};
+
+export { loader };
 export default ProductsPage;
